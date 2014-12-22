aa43c8d354e68955ea77214898f3c8e64549bf96
torkale <torkale@gmail.com>
2014-12-22 11:16:51 +0200

    updates to Streaming

--- video_grep.sh
+++ video_grep.sh
@@ -8,7 +8,7 @@
   #-profile:v baseline \ 264 profile
   #-level:v 3.0 \ 264 level
   #-b:v 400K \ video bit rate 400 kb/s
-  #-r 25 \ frame rate 12 fps
+  #-r 25 \ frame rate 25 fps
   #-g 300 \ The -g option is the "group of pictures" (GOP) size, which is the number of frames between keyframes.  With a smaller number, your output will have more keyframes, which means that streaming clients will be able to recover more quickly if they drop packets for some reason.  It also will have a detrimental effect on file size.
   #-s 480x270 \ frame size WxH
   #-f hls \ http live streaming format
@@ -29,33 +29,23 @@
   #-reset_timestamps 1 \ Reset timestamps at the begin of each segment, so that each segment will start with near-zero timestamps.
   #public/mp4/out%02d.mp4 /mp4 output destination
 ffmpeg \
+  -f avfoundation \
+  -i "default" \
   -c:v libx264 \
   -c:a copy \
   -pix_fmt yuv420p \
   -profile:v baseline \
   -level:v 3.0 \
-  -b:v 400K \
-  -r 25 \
+  -b:v 300K \
+  -r 15 \
   -g 300 \
   -s 1280x720 \
-  -f hls \
-  -hls_time 1 \
-  public/hls/mystream.m3u8 \
-  -c:v libx264 \
-  -c:a copy \
-  -pix_fmt yuv420p \
-  -profile:v baseline \
-  -level:v 3.0 \
-  -b:v 400K \
-  -r 25 \
-  -g 300 \
-  -s 1280x720 \
-  -f segment \
-  -segment_time 20 \
-  -reset_timestamps 1 \
-  public/mp4/out%02d.mp4
-  #-f avfoundation \
-  #-i "default" \
+  -f rtp rtp://10.100.102.8:1234
+  #-f hls \
+  #-hls_time 1 \
+  #-hls_wrap 10 \
+  #-hls_list_size 0 \
+  #public/hls/mystream.m3u8 \
   #-c:v libx264 \
   #-c:a copy \
   #-pix_fmt yuv420p \
@@ -67,3 +57,16 @@ ffmpeg \
   #-s 1280x720 \
   #-f flv \
   #rtmp://localhost:1935/hls/movie
+  #-c:v libx264 \
+  #-c:a copy \
+  #-pix_fmt yuv420p \
+  #-profile:v baseline \
+  #-level:v 3.0 \
+  #-b:v 400K \
+  #-r 25 \
+  #-g 300 \
+  #-s 1280x720 \
+  #-f segment \
+  #-segment_time 20 \
+  #-reset_timestamps 1 \
+  #public/mp4/out%02d.mp4
